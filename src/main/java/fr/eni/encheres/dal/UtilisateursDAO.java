3c2eb0fe8076dd5dac71abd17c0e61ec82368cc6
Meithal <hurin8888@gmail.com>
2021-12-06 20:58:34 +0100

    Add implem utilisateur.

--- src/main/java/fr/eni/encheres/dal/UtilisateursDAO.java
+++ src/main/java/fr/eni/encheres/dal/UtilisateursDAO.java
@@ -1,5 +1,14 @@
 package fr.eni.encheres.dal;
 
+import java.util.List;
+
+import fr.eni.encheres.bo.Utilisateur;
+
 public interface UtilisateursDAO {
 
+	public Utilisateur getUtilisateurById(int id) throws DALException;
+	public List<Utilisateur> getAllUtilisateurs() throws DALException;
+	public void addUtilisateur(Utilisateur utilisateur) throws DALException;
+	public void updateUtilisateur(Utilisateur utilisateur) throws DALException;
+	public void removeUtilisateur(Utilisateur utilisateur) throws DALException;
 }
