ba3c157b45faa320f49faf6f20b75f8295a120c5
zoran <zoran@LAPTOP-JKURSJPU.home>
2021-12-07 14:25:23 +0100

    Merge branch 'master' of https://github.com/Projet-java-eni/Projet.git

--- src/main/java/fr/eni/encheres/dal/RetraitsDAO.java
+++ src/main/java/fr/eni/encheres/dal/RetraitsDAO.java
@@ -3,7 +3,6 @@ package fr.eni.encheres.dal;
 import fr.eni.encheres.bo.ArticleVendu;
 
 public interface RetraitsDAO {
-	
-	
+		
 	public static final ArticleVendu sqlSelectCoordonnees throws DALException;
 }
