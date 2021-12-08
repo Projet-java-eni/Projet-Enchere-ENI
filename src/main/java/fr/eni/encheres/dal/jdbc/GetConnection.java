f7932e862f28734685794c579bfdc62ddcdda8b1
zoran <zoran@LAPTOP-JKURSJPU.home>
2021-12-08 22:17:36 +0100

    MAJ GetConnection close

--- src/main/java/fr/eni/encheres/dal/jdbc/GetConnection.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/GetConnection.java
@@ -11,9 +11,10 @@ import javax.sql.DataSource;
 import fr.eni.encheres.dal.DALException;
 
 class GetConnection {
-    public static Connection getConnexion() throws DALException {
-    	Context context;
-    	DataSource dataSource;
+
+	public static Connection getConnexion() throws DALException {
+		Context context;
+		DataSource dataSource;
 		try {
 			context = new InitialContext();
 			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
@@ -21,5 +22,17 @@ class GetConnection {
 		} catch (NamingException | SQLException e1) {
 			throw new DALException(e1.getMessage(), e1);
 		}
-    }
+	}
+
+// fermeture de la connexion à la BDD
+	static void close(AutoCloseable resource) throws DALException {
+		if (resource != null) {
+
+			try {
+				resource.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 }
