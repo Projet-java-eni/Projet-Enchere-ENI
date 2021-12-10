5e4f9fe8fb2f4262f3a3ee7a423958fd34baf08e
LULU <LULU@192.168.1.48>
2021-12-10 10:56:39 +0100

    MAJ généricité DAOFactory

--- src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -1,6 +1,11 @@
+//MAJ Lucie (généricité)
 package fr.eni.encheres.dal;
 
+import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Categorie;
+import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.bo.Retrait;
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.jdbc.ArticlesVendusImpl;
 import fr.eni.encheres.dal.jdbc.CategoriesImpl;
 import fr.eni.encheres.dal.jdbc.EncheresImpl;
@@ -8,15 +13,15 @@ import fr.eni.encheres.dal.jdbc.RetraitsImpl;
 import fr.eni.encheres.dal.jdbc.UtilisateursImpl;
 
 public class DAOFactory {
-	private static ArticlesVendusDAO articlesVendusDAO = null;
+	private static DAO<ArticleVendu> articlesVendusDAO = null;
 	private static DAO<Categorie> categoriesDAO = null;
-	private static EncheresDAO encheresDAO = null;
-	private static RetraitsDAO retraitsDAO = null;
-	private static UtilisateursDAO utilisateursDAO = null;
+	private static DAO<Enchere> encheresDAO = null;
+	private static DAO<Retrait> retraitsDAO = null;
+	private static DAO<Utilisateur> utilisateursDAO = null;
     private DAOFactory() {
     }
 
-    public static ArticlesVendusDAO getArticlesVendusDAO() throws DALException {
+    public static DAO<ArticleVendu> getArticlesVendusDAO() throws DALException {
     	if(articlesVendusDAO == null) {
     		articlesVendusDAO = new ArticlesVendusImpl();
     	}
@@ -30,21 +35,21 @@ public class DAOFactory {
         return categoriesDAO;
     }
     
-    public static EncheresDAO getEncheresDAO() throws DALException {
+    public static DAO<Enchere> getEncheresDAO() throws DALException {
     	if(encheresDAO == null) {
     		encheresDAO = new EncheresImpl();
     	}
         return encheresDAO;
     }
     
-    public static RetraitsDAO getRetraitsDAO() throws DALException {
+    public static DAO<Retrait> getRetraitsDAO() throws DALException {
     	if(retraitsDAO == null) {
     		retraitsDAO = new RetraitsImpl();
     	}
         return retraitsDAO;
     }
     
-    public static UtilisateursDAO getUtilisateursDAO() throws DALException {
+    public static DAO<Utilisateur> getUtilisateursDAO() throws DALException {
     	if(utilisateursDAO == null) {
     		utilisateursDAO = new UtilisateursImpl();
     	}
