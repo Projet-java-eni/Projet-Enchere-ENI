6605fc1b02f6839d5dcf5488e6b3f6d5bea4ac1f
LULU <LULU@192.168.1.48>
2021-12-10 11:32:09 +0100

    MAJ DAO.java liste méthodes

--- src/main/java/fr/eni/encheres/dal/DAO.java
+++ src/main/java/fr/eni/encheres/dal/DAO.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public interface DAO<T> {
 	public List<T> getAll() throws DALException;
-	public void add(T categorie) throws DALException;
-	public void update(T utilisateur) throws DALException;
-	public void remove(T utilisateur) throws DALException;
+	public void add(T t) throws DALException;
+	public void update(T t) throws DALException;
+	public void remove(T t) throws DALException;
 
 }
