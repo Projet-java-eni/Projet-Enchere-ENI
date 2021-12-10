50dc860c54e6295b85bd9389593e8b0a65f330a7
Sego <zoran@LAPTOP-JKURSJPU.home>
2021-12-10 16:34:52 +0100

    MAJ Categorie

--- src/main/java/fr/eni/encheres/bo/Categorie.java
+++ src/main/java/fr/eni/encheres/bo/Categorie.java
@@ -1,26 +1,28 @@
 package fr.eni.encheres.bo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-
-public class Categorie implements Serializable { 
+public class Categorie implements Serializable, MeHasMany<ArticleVendu> {
 
 	@Override
 	public String toString() {
-		return String.format(
-				"Utilisateur : id %d, libelle : %s", noCategorie, libelle
-		);
+		return String.format("Utilisateur : id %d, libelle : %s", noCategorie, libelle);
 	}
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1;
-	
+
+	// champs de la BDD
 	private int noCategorie;
 	private String libelle;
-	
-	
+
+	// champs supplémentaires
+	private List<ArticleVendu> categorieArticle = null;
+
 	public Categorie(int id, String libelle) {
 		super();
 		this.noCategorie = id;
@@ -31,21 +33,34 @@ public class Categorie implements Serializable {
 		this(-1, libelle);
 	}
 
-	
 	public int getId() {
 		return noCategorie;
 	}
-	
+
 	public void setId(int id) {
 		this.noCategorie = id;
 	}
-	
+
 	public String getLibelle() {
 		return libelle;
 	}
-	
+
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
 	}
 
+	// Création de méthodes permettant de faire le lien avec la classe ArticleVendu
+	@Override
+	public void ajouter(ArticleVendu typeArticle) {
+		if (categorieArticle == null) {
+			categorieArticle = new ArrayList<>();
+		}
+
+		categorieArticle.add(typeArticle);
+
+	}
+
+	public void supprimer(ArticleVendu typeArticle) {
+		categorieArticle.remove(typeArticle);
+	}
 }
