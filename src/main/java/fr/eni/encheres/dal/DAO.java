dfcbcd8f79a61d755b03ebc84737e6a22b4f933f
LULU <LULU@192.168.1.48>
2021-12-10 11:23:42 +0100

    MAJ DAO retrait méthode getById

--- src/main/java/fr/eni/encheres/dal/DAO.java
+++ src/main/java/fr/eni/encheres/dal/DAO.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 
 public interface DAO<T> {
-	public T getById(int id) throws DALException;
 	public List<T> getAll() throws DALException;
 	public void add(T categorie) throws DALException;
 	public void update(T utilisateur) throws DALException;
