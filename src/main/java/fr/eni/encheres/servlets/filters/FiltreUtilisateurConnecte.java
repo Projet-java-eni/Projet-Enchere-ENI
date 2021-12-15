a7da8b58ba2a640fdde7f3ea1e82751a371c7ae4
Fredericjean <fred42110@hotmail.fr>
2021-12-15 15:43:00 +0100

    filtre session

---
+++ src/main/java/fr/eni/encheres/servlets/filters/FiltreUtilisateurConnecte.java
@@ -0,0 +1,31 @@
+package fr.eni.encheres.servlets.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(filterName = "FiltreUtilisateurConnecté","urlPatterns="//décider quelles url seront concernées ")
+		
+public class FiltreUtilisateurConnecte implements Filter {
+
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+		if(httpServletRequest.getSession().getAttribute("user_id") == null) {
+			httpServletResponse.sendRedirect(httpServletRequest.getContextPath()+"/Accueil");
+		}
+
+		else chain.doFilter(request, response);
+	}
+}
+
+}
